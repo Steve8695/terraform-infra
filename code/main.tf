abf826694e7c9e07f6619f6f84ac183c0d57c2cc
Steve8695 <51990522+Steve8695@users.noreply.github.com>
2022-09-21 20:50:18 +0530

    updated source git

--- code/main.tf
+++ code/main.tf
@@ -1,24 +1,24 @@
 module "azurerm_resource_group" {
-  source           = "../infra-modules/rg"
+  source           = "github.com/Steve8695/terraform-infra.git//infra-modules//rg"
   resource_group   = "testkjdsk"
   location         = "eastus"
 }
 
 module "azurerm_virtual_network" {
-  source         = "../infra-modules/vnet"
+  source         = "github.com/Steve8695/terraform-infra.git//infra-modules//vnet"
   resource_group = module.azurerm_resource_group.resource_group
   vnet           = "steve-vnet"
 }
 
 
 module "azurerm_network_security_group" {
-  source         = "../infra-modules/nsg"
+  source         = "github.com/Steve8695/terraform-infra.git//infra-modules//nsg"
   resource_group = module.azurerm_resource_group.resource_group
   sg_name        = "steve-sg"
 }
 
 module "vmss" {
-  source                    = "../infra-modules/vmss"
+  source                    = "github.com/Steve8695/terraform-infra.git//infra-modules//vmss"
   location                  = "eastus"
   resource_group            = module.azurerm_resource_group.resource_group
   packer_resource_group_name= "packerGroup"
