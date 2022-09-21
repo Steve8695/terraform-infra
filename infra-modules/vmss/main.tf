d0cfac1354424c8c6017f7eafb0647fa22c32193
Steve8695 <51990522+Steve8695@users.noreply.github.com>
2022-09-21 23:22:14 +0530

    updated key data

--- infra-modules/vmss/main.tf
+++ infra-modules/vmss/main.tf
@@ -78,65 +78,41 @@ data "azurerm_image" "image" {
   resource_group_name = data.azurerm_resource_group.image.name
 }
 
-resource "azurerm_virtual_machine_scale_set" "vmss" {
+
+
+
+
+resource "azurerm_windows_virtual_machine_scale_set" "vmss" {
   name                = "vmscaleset"
   location            = var.location
   resource_group_name = var.resource_group
-  upgrade_policy_mode = "Manual"
-
-  sku {
-    name     = "Standard_DS1_v2"
-    tier     = "Standard"
-    capacity = 2
+  admin_username       =  var.admin_user
+  admin_password       = var.admin_password
+  instances = 2
+  computer_name_prefix = "devops"
+  sku = "Standard_F2"
+  os_disk {
+    caching = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
 
-  storage_profile_image_reference {
-    id=data.azurerm_image.image.id
-  }
+  source_image_id = data.azurerm_image.image.id
 
-  storage_profile_os_disk {
-    name              = ""
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  storage_profile_data_disk {
-    lun          = 0
-    caching        = "ReadWrite"
-    create_option  = "Empty"
-    disk_size_gb   = 10
-  }
-
-  os_profile {
-    computer_name_prefix = "vmlab"
-    admin_username       = var.admin_user
-    admin_password       = var.admin_password
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = true
-
-    ssh_keys {
-      path     = "/home/azureuser/.ssh/authorized_keys"
-      key_data = file("~/.ssh/steve-key.pub")
-    }
-  }
-
-  network_profile {
+  network_interface {
     name    = "terraformnetworkprofile"
     primary = true
 
     ip_configuration {
-      name                                   = "IPConfiguration"
-      subnet_id                              = var.subnet
+      name              = "IPConfiguration"
+      subnet_id         = var.subnet
       load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.bpepool.id]
       primary = true
     }
   }
-  
-  tags = var.tags
 }
+
+
+
 
 resource "azurerm_public_ip" "jumpbox" {
   name                         = "jumpbox-public-ip"
@@ -190,13 +166,9 @@ resource "azurerm_virtual_machine" "jumpbox" {
   }
 
   os_profile_linux_config {
-    disable_password_authentication = true
-
-    ssh_keys {
-      path     = "/home/azureuser/.ssh/authorized_keys"
-      key_data = file("~/.ssh/steve-key.pub")
-    }
+    disable_password_authentication = false
   }
 
   tags = var.tags
 }
+
