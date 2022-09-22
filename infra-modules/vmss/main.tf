c9833e3b0b1331e32785bdb22635f498303344ba
Steve8695 <51990522+Steve8695@users.noreply.github.com>
2022-09-22 12:10:11 +0530

    Update main.tf

--- infra-modules/vmss/main.tf
+++ infra-modules/vmss/main.tf
@@ -138,37 +138,5 @@ resource "azurerm_network_interface" "jumpbox" {
   tags = var.tags
 }
 
-resource "azurerm_virtual_machine" "jumpbox" {
-  name                  = "jumpbox"
-  location              = var.location
-  resource_group_name   = var.resource_group
-  network_interface_ids = [azurerm_network_interface.jumpbox.id]
-  vm_size               = "Standard_DS1_v2"
 
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-
-  storage_os_disk {
-    name              = "jumpbox-osdisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  os_profile {
-    computer_name  = "jumpbox"
-    admin_username = var.admin_user
-    admin_password = var.admin_password
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-  tags = var.tags
-}
 
