3c843b660b93c65def90cf96bf60b697aef7e589
Steve8695 <51990522+Steve8695@users.noreply.github.com>
2022-09-22 22:12:57 +0530

    Update main.tf

--- code/main.tf
+++ code/main.tf
@@ -1,28 +1,28 @@
 module "azurerm_resource_group" {
   source           = "github.com/Steve8695/terraform-infra.git//infra-modules//rg"
-  resource_group   = "testkjdsk"
-  location         = "eastus"
+  resource_group   = var.resource_group
+  location         = var.location
 }
 
 module "azurerm_virtual_network" {
   source         = "github.com/Steve8695/terraform-infra.git//infra-modules//vnet"
   resource_group = module.azurerm_resource_group.resource_group
-  vnet           = "steve-vnet"
+  vnet           = var.vnet_name
 }
 
 
 module "azurerm_network_security_group" {
   source         = "github.com/Steve8695/terraform-infra.git//infra-modules//nsg"
   resource_group = module.azurerm_resource_group.resource_group
-  sg_name        = "steve-sg"
+  sg_name        = var.sg_name
 }
 
 module "vmss" {
   source                    = "github.com/Steve8695/terraform-infra.git//infra-modules//vmss"
-  location                  = "eastus"
+  location                  = var.location
   resource_group            = module.azurerm_resource_group.resource_group
-  packer_resource_group_name= "packerGroup"
-  packer_image_name         = "myPackerImage"
-  admin_password            = "Darkknight@123"
+  packer_resource_group_name= var.packer_resource_group_name
+  packer_image_name         = var.packer_image_name
+  admin_password            = var.admin_password
   subnet                    = module.azurerm_virtual_network.subnet_id
   }
